fdba3fc3fd6dd486df6689eb8f8527915997620c
RhytmicRider <devos.alex.11@gmail.com>
2025-11-08 10:44:55 +0100

    8/11

--- startcode/a_eerste_flask_app/a_eerste_flask_app.py
+++ startcode/a_eerste_flask_app/a_eerste_flask_app.py
@@ -1,10 +1,33 @@
-from flask import Flask
+from django.shortcuts import render
+from flask import Flask, render_template
+
+uitgenodigde = ["Ik"]
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "<h1>Hallo wereld!</h1><p>Dit is mijn eerste Flask app</p>"
+    return render_template("Home.html")
+
+@app.route("/over")
+def over():
+    return render_template("over.html")
+@app.route("/uitnodiging/<naam>")
+def uitnodiging(naam):
+    if naam in uitgenodigde:
+        return render_template(
+            "uitgenodiging.html",
+                                naam=naam,
+            locatie = "70.974952, 3.259998",
+            datum= "19 oktober 1845"
+
+
+        )
+    else:
+        return render_template("uitgenodigd.html",
+                                datum= "Nope",
+                                locatie= "YOU'RE MOM")
+
 
 if __name__ == "__main__":
    app.run(debug=True)
